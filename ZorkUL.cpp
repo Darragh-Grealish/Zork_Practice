80921a16788be5df7d61a9febd7b6134c7728788
Darragh-Grealish <dgrealish2003@gmail.com>
2023-05-02 21:27:13 +0100

    Initial Commit

---
+++ ZorkUL.cpp
@@ -0,0 +1,196 @@
+#include <iostream>
+
+using namespace std;
+#include "ZorkUL.h"
+
+int main(int argc, char *argv[]) {
+    ZorkUL temp;
+	temp.play();
+	return 0;
+}
+
+ZorkUL::ZorkUL() {
+	createRooms();
+}
+
+void ZorkUL::createRooms()  {
+    Room *aRoom, *b, *c, *d, *e, *f, *g, *h, *i, *newRoom;
+
+    aRoom = new Room("aRoom");
+        aRoom->addItem(new Item("x", 1, 11));
+        aRoom->addItem(new Item("y", 2, 22));
+	b = new Room("b");
+        b->addItem(new Item("xx", 3, 33));
+        b->addItem(new Item("yy", 4, 44));
+	c = new Room("c");
+	d = new Room("d");
+	e = new Room("e");
+	f = new Room("f");
+	g = new Room("g");
+	h = new Room("h");
+    i = new Room("i");
+    newRoom = new Room("newRoom"); //Desription, inWeight, inValue (found in 'item.h' file)
+        newRoom->addItem(new Item("newItem", 5, 55));
+
+
+    //             (N, E, S, W)
+    aRoom->setExits(f, b, d, c);
+    b->setExits(NULL, NULL, NULL, aRoom);
+    c->setExits(NULL, aRoom, NULL, NULL);
+    d->setExits(aRoom, e, NULL, i);
+    e->setExits(NULL, NULL, NULL, d);
+    f->setExits(newRoom, g, aRoom, h);
+    g->setExits(NULL, NULL, NULL, f);
+    h->setExits(NULL, f, NULL, NULL);
+    i->setExits(NULL, d, NULL, NULL);
+    newRoom->setExits(NULL, NULL, f, NULL);
+
+    currentRoom = aRoom; // A pointer to the current (starting room)
+}
+
+/**
+ *  Main play routine.  Loops until end of play.
+ */
+void ZorkUL::play() {
+	printWelcome();
+
+	// Enter the main command loop.  Here we repeatedly read commands and
+	// execute them until the ZorkUL game is over.
+
+	bool finished = false;
+	while (!finished) {
+		// Create pointer to command and give it a command.
+		Command* command = parser.getCommand();
+		// Pass dereferenced command and check for end of game.
+		finished = processCommand(*command);
+		// Free the memory allocated by "parser.getCommand()"
+		//   with ("return new Command(...)")
+		delete command;
+	}
+	cout << endl;
+	cout << "end" << endl;
+}
+
+void ZorkUL::printWelcome() {
+	cout << "start"<< endl;
+	cout << "info for help"<< endl;
+	cout << endl;
+	cout << currentRoom->longDescription() << endl;
+}
+
+/**
+ * Given a command, process (that is: execute) the command.
+ * If this command ends the ZorkUL game, true is returned, otherwise false is
+ * returned.
+ */
+
+bool ZorkUL::processCommand(Command command) {
+    if (command.isUnknown()) {
+        cout << "invalid input --"<< endl;
+        return false;
+    }
+
+
+	string commandWord = command.getCommandWord();
+	if (commandWord.compare("info") == 0)
+		printHelp();
+
+	else if (commandWord.compare("map") == 0)
+		{
+        cout << "     [newRoom]     " << endl;
+        cout << "         |         " << endl;
+        cout << "         |         " << endl;
+        cout << "[h] --- [f] --- [g]" << endl;
+		cout << "         |         " << endl;
+        cout << "         |         " << endl;
+        cout << "[c] - [aRoom] - [b]" << endl;
+		cout << "         |         " << endl;
+		cout << "         |         " << endl;
+		cout << "[i] --- [d] --- [e]" << endl;
+		}
+
+	else if (commandWord.compare("go") == 0)
+		goRoom(command);
+
+    else if (commandWord.compare("take") == 0)
+    {
+       	if (!command.hasSecondWord()) {
+        cout << "incomplete input"<< endl;
+        }
+        else
+         if (command.hasSecondWord()) {
+        cout << "you're trying to take " + command.getSecondWord() << endl;
+        int location = currentRoom->isItemInRoom(command.getSecondWord());
+        if (location  < 0 )
+            cout << "item is not in room" << endl;
+        else
+            cout << "item is in room" << endl;
+             cout << "index number " << + location << endl;
+            cout << endl;
+            cout << currentRoom->longDescription() << endl;
+        }
+    }
+
+    else if (commandWord.compare("put") == 0)
+    {
+
+    }
+    /*
+    {
+    if (!command.hasSecondWord()) {
+		cout << "incomplete input"<< endl;
+        }
+        else
+            if (command.hasSecondWord()) {
+            cout << "you're adding " + command.getSecondWord() << endl;
+            itemsInRoom.push_Back;
+        }
+    }
+*/
+    else if (commandWord.compare("quit") == 0) {
+		if (command.hasSecondWord())
+			cout << "overdefined input"<< endl;
+		else
+			return true; /**signal to quit*/
+	}
+	return false;
+}
+/** COMMANDS **/
+void ZorkUL::printHelp() {
+	cout << "valid inputs are; " << endl;
+	parser.showCommands();
+
+}
+
+void ZorkUL::goRoom(Command command) {
+	if (!command.hasSecondWord()) {
+		cout << "incomplete input"<< endl;
+		return;
+	}
+
+	string direction = command.getSecondWord();
+
+	// Try to leave current room.
+	Room* nextRoom = currentRoom->nextRoom(direction);
+
+	if (nextRoom == NULL)
+		cout << "underdefined input"<< endl;
+	else {
+		currentRoom = nextRoom;
+		cout << currentRoom->longDescription() << endl;
+	}
+}
+
+string ZorkUL::go(string direction) {
+	//Make the direction lowercase
+	//transform(direction.begin(), direction.end(), direction.begin(),:: tolower);
+	//Move to the next room
+	Room* nextRoom = currentRoom->nextRoom(direction);
+	if (nextRoom == NULL)
+		return("direction null");
+	else
+	{
+		currentRoom = nextRoom;
+		return currentRoom->longDescription();
+	}
+}
---
+++ item.h
@@ -0,0 +1,39 @@
+// Inclusion guard, 'if file is included, don't include it again'
+// Defines ITEM_H_ first, second time it goes to include the file, it won't include it
+#ifndef ITEM_H_
+#define ITEM_H_
+
+// Standard header files from the standard header file that come with C++
+// Angle brackets tells compiler to look in C++ library?, "" to look in local directory
+#include <map>
+#include <string>
+#include <iostream>
+using namespace std;
+
+class Item {
+private: //Private encapsulated data, a description of the inetrface
+    string description; //Axe
+    string longDescription; //Old axe with old scripture,
+    int weightGrams; //Maybe have a carry limit
+    float value; //Tradable...?
+    bool weaponCheck; //Is it extendable, a wepon, clean
+
+public:
+    Item (string description, int inWeight, float inValue); //Constructor
+    Item (string description);
+	string getShortDescription();
+    string getLongDescription();
+	int getWeight();
+	void setWeight(int weightGrams);
+	float getValue();
+	void setValue(float value);
+	int getWeaponCheck();
+	void setWeaponCheck(int weaponCheck);
+};
+
+// # is a preprocessor language, series of commands that let you edit the code before compiling, "replace this with this"
+// can give the same software to all phone types, but you can say to use certain code for different phone types
+
+#endif /*ITEM_H_*/
+// Inclusion guard
+
